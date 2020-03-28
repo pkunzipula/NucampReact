93730484ae63cba6194f318254fc7e834917e739
pkunzipula <jcapra76@gmail.com>
2020-03-28 09:40:43 -0700

    Adding to the Single Page App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Navbar, NavbarBrand, Nav } from "reactstrap";
 import { CAMPSITES } from "./shared/campsites";
+import { BrowserRouter } from "react-router-dom";
 import Directory from "./components/DirectoryComponent";
 import "./App.css";
 import Main from "./components/MainComponent";
@@ -8,9 +9,11 @@ import Main from "./components/MainComponent";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
